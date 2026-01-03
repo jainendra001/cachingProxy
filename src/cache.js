4383f1ab7e8004ba7a53f0787ae1695baf8cfc46
Jainendra Tripathy <jainendratripathy@gmail.com>
2026-01-03 17:17:18 +0530

    The logic for the LRU cache evitction is added for memory pressue

--- src/cache.js
+++ src/cache.js
@@ -1,5 +1,6 @@
 const cache=new Map()
 const DEFAULT_TTL=30*1000
+const MAX_CACHE_SIZE=100
 
 function get(key){
   const entry=cache.get(key)
@@ -11,10 +12,20 @@ function get(key){
     return null;
   }
   console.log(`Cache is not missed and the data is upto date!!!`)
+  cache.delete(key);
+  cache.set(key, entry);
   return entry;
 }
 
 function set(key,value,ttl=DEFAULT_TTL){
+  if(cache.has(key)){
+    cache.delete(key)
+  }
+
+  if(cache.size>=MAX_CACHE_SIZE){
+    const lruKey=cache.keys().next().value;
+    cache.delete(lruKey)
+  }
   cache.set(key,{
     ...value,
     createdAt:Date.now(),
@@ -27,8 +38,13 @@ function clear(){
   cache.clear()
 }
 
+function size(){
+  return cache.size;
+}
+
 module.exports={
   get,
   set,
-  clear
+  clear,
+  size
 }
