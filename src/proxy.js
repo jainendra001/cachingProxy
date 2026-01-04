2c37be8bc3c0211ac48376c60e6c9b898b6791b8
Jainendra Tripathy <jainendratripathy@gmail.com>
2026-01-04 11:05:56 +0530

    The security checks for the caching is maintained

--- src/proxy.js
+++ src/proxy.js
@@ -7,7 +7,49 @@ function getCacheKey(req) {
   return `${req.method}:${req.url}`;
 }
 
+function isCacheableMethod(method) {
+  return method === "GET" || method === "HEAD";
+}
+function hasAuthHeaders(req) {
+  return Boolean(
+    req.headers["authorization"] ||
+    req.headers["cookie"]
+  );
+}
+
+function forwardWithoutCache(clientReq,clientRes,origin){
+  const originURL=new URL(origin);
+  const options={
+    protocol:originURL.protocol,
+    hostname:originURL.hostname,
+    path:clientReq.url,
+    method:clientReq.method,
+    port: originUrl.port || (originUrl.protocol === "https:" ? 443 : 80),
+    headers:{
+      ...clientReq.headers,
+      host:originURL.host
+    }
+  };
+  const transport=originURL.protocol==='https:'?https:http
+  const proxyReq=transport.request(options,(proxyRes)=>{
+    clientRes.writeHead(proxyRes.statusCode,{
+      ...proxyRes.headers,
+      "X-Cache": "BYPASS",
+    });
+    proxyRes.pipe(clientRes);
+  });
+  clientReq.pipe(proxyReq);
+}
+
+
 function forwardRequest(clientReq, clientRes, origin) {
+  
+  // 🚫 Do not cache unsafe methods
+  if (!isCacheableMethod(clientReq.method) || hasAuthHeaders(clientReq)) {
+    console.log(`Caching is not allowed for this method or it contains auth!!!!`)
+  return forwardWithoutCache(clientReq, clientRes, origin);
+  }
+
   const cacheKey = getCacheKey(clientReq);
   const cachedResponse = cache.get(cacheKey);
 
@@ -17,7 +59,7 @@ function forwardRequest(clientReq, clientRes, origin) {
       ...cachedResponse.headers,
       "X-Cache": "HIT",
     });
-
+    console.log(`We got the cache, it is cache hit!!`)
     clientRes.end(cachedResponse.body);
     return;
   }
@@ -50,19 +92,27 @@ function forwardRequest(clientReq, clientRes, origin) {
     proxyRes.on("end", () => {
       const body = Buffer.concat(chunks);
 
+      const cacheControl = proxyRes.headers["cache-control"];
+
+      const shouldCache =
+        !cacheControl ||
+        (!cacheControl.includes("no-store") &&
+        !cacheControl.includes("no-cache") &&
+        !cacheControl.includes("private"));
+
       // Store in cache
-      cache.set(
-        cacheKey, 
-        {
-        statusCode: proxyRes.statusCode,
-        headers: proxyRes.headers,
-        body,
-        }
-    );
+      if (shouldCache) {
+        console.log(`We should respect the cache controller`)
+        cache.set(cacheKey, {
+          statusCode: proxyRes.statusCode,
+          headers: proxyRes.headers,
+          body,
+        });
+      }
 
       clientRes.writeHead(proxyRes.statusCode, {
         ...proxyRes.headers,
-        "X-Cache": "MISS",
+        "X-Cache": shouldCache ? "MISS" : "BYPASS",
       });
 
       clientRes.end(body);
